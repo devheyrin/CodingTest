91feca4f0d8b347ac28dc9f36c4fdc191ecbc749
Heyrin <77548431+devheyrin@users.noreply.github.com>
2022-06-13 15:41:52 +0900

    Create is_prime.py

---
+++ Programmers/is_prime.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    for i in range(2, n+1):
+        if isPrime(i):
+            answer += 1
+    return answer
+
+
+def isPrime(num):
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
