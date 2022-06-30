5aa9c5a3fbc95ca5e1c7ea66a037f73df02f5664
Heyrin <77548431+devheyrin@users.noreply.github.com>
2022-06-30 17:57:52 +0900

    Update chap4-3.py

--- ThisIsCodingTest/chap4-3.py
+++ ThisIsCodingTest/chap4-3.py
@@ -7,58 +7,49 @@ graph = []
 for _ in range(n):
   graph.append(list(map(int, input().split())))
 
+visited = [[0]*m for _ in range(n)]
+
 # 방문 가능 칸
 steps = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1)}
 
 graph[x][y] = 1 # 시작점 방문 처리 
 
 visit_cnt = 1
+turn_time = 0
 
-def turn_left(d):
-  return (d + 1) % 4
+# 왼쪽으로 회전 
+def turn_left():
+  global d
+  d -= 1
+  if d == -1:
+    d = 3
 
 while True:
-  for _ in range(4):
-    left = turn_left(d)
-    dx, dy = steps[left]
-    nx, ny = x + dx, y + dy
-
-    # 바다로 되어 있는 칸이면 pass
-    if nx < 0 or nx >= n or ny < 0 or ny >= m:
-      continue
-
-    # 이미 가본 칸이거나 바다로 되어 있는 칸이면 pass
-    if graph[nx][ny] == 1:
-      continue
-
-    # 한 칸 전진 
-    x, y = nx, ny
-    # 왼쪽으로 회전 
-    d = left
-    # 방문 처리 
-    graph[x][y] = 1
-    visit_cnt += 1
-
-  # 네 방향 모두 이미 가본 칸이거나 바다로 되어 있는 경우 
+  turn_left() 
   dx, dy = steps[d]
-  nx, ny = x - dx, y - dy
+  nx, ny = x + dx, y + dy
 
-  # 바다여서 갈 수 없으면 break
-  if nx < 0 or nx >= n or ny < 0 or ny >= m:
-    break
-  if graph[nx][ny] == 1:
-    break
+  if graph[nx][ny] == 0 and visited[nx][ny] == 0:
+    x, y = nx, ny
+    # 방문 처리 
+    visited[x][y] = 1
+    visit_cnt += 1
+    turn_time = 0
+    continue
 
-  # 한 칸 뒤로 
-  x, y = nx, ny
-  # 방문 처리 
-  graph[x][y] = 1
-  visit_cnt += 1
+  else:
+    turn_time += 1
+  
+  if turn_time == 4:
+    # 네 방향 모두 이미 가본 칸이거나 바다로 되어 있는 경우 
+    dx, dy = steps[d]
+    nx, ny = x - dx, y - dy
+
+    if graph[nx][ny] == 0:
+      x, y = nx, ny
+    # 한 칸 뒤로 
+    else:
+      break
+    turn_time = 0
 
 print(visit_cnt)
-
-
-  
-
-
-
